e010bc293f39c7b10513bae79fb184b49d99a5bf
Zelin Li <zelin.l@foxmail.com>
2023-04-21 17:32:30 +0200

    Update string_API_shortest_path.sh

--- string_API_shortest_path.sh
+++ string_API_shortest_path.sh
@@ -3,5 +3,5 @@
 # Latest download: https://string-db.org/cgi/download?sessionId=bIJIOttT3oi6
 wget https://stringdb-static.org/download/protein.physical.links.v11.5/9606.protein.physical.links.v11.5.txt.gz
 gunzip 9606.protein.physical.links.v11.5.txt.gz
-./string_API_shortest_path.py 9606.protein.physical.links.v11.5.txt $1 $2
+python3.9 string_API_shortest_path.py 9606.protein.physical.links.v11.5.txt $1 $2
 rm -rf 9606.protein.physical.links.v11.5.txt
